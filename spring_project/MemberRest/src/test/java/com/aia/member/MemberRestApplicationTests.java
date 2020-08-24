e8ad8c17b5b1c48f083bb79ae8b7cc3a09ca3e83
clachic <anajrwk2@gmail.com>
2020-08-24 15:51:03 +0900

    .

--- spring_project/MemberRest/src/test/java/com/aia/member/MemberRestApplicationTests.java
+++ spring_project/MemberRest/src/test/java/com/aia/member/MemberRestApplicationTests.java
@@ -16,7 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class MemberRestApplicationTests {
+public class MemberRestApplicationTests {
 
 	@Autowired
 	private DataSource dataSource;
